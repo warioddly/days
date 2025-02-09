92a04fc58c88a43db07c7d7988f0180e344dbcd0
bekey <53710487+warioddly@users.noreply.github.com>
2025-02-09 23:38:59 +0600

    Update dot_list_body.dart

--- lib/features/home/presentation/widgets/dot/dot_list_body.dart
+++ lib/features/home/presentation/widgets/dot/dot_list_body.dart
@@ -48,7 +48,7 @@ class _GridListBodyState extends State<GridListBody> {
                         to: settings.endDateTime,
                         lengthCalculate: settings.gridType.calculation,
                         dayCalculate: settings.gridType.calculationDay,
-                        padding: Dimensions.doubleNormal.padding.copyWith(
+                        padding: Dimensions.doubledNormal.padding.copyWith(
                           top: (Dimensions.dotContainerSize * 2) -
                               (Dimensions.dotSize * 2) + safeAreaPaddingTop,
                         ),
