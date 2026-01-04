c1727becec80700226945d8856e8a02930324ff2
IMØ <53710487+warioddly@users.noreply.github.com>
2026-01-04 15:57:06 +0600

    Update dimensions.dart

--- lib/shared/ui/dimensions/dimensions.dart
+++ lib/shared/ui/dimensions/dimensions.dart
@@ -12,6 +12,7 @@ final class Dimensions {
   static const double ms = 10.0;
   static const double sm = 12.0;
   static const double m = 16.0;
+  static const double msl = 16.0;
   static const double ml = 20.0;
   static const double l = 24.0;
   static const double xl = 32.0;
